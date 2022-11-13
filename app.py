af66a3d7f865bfa16722efab05a8c50be83b3adb
Kenny La <kenny.la@outlook.com>
2022-11-13 02:38:18 +0100

    Some templating

--- app.py
+++ app.py
@@ -3,11 +3,31 @@ from flask import render_template
 
 app = Flask(__name__)
 
+navigation = {
+    "Home": "Home"
+}
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template("start_page.html")
+@app.route('/home')
+def home():  # put application's code here
+    # TODO: add auth
+    user = "Kenny"
+    return render_template("start_page.html", user=user)
 
+
+@app.route("/all-topics")
+def all_topics():
+    return render_template("show_all_topics.html")
+
+
+@app.route("/progress")
+def progress():
+    return render_template("progress.html")
+
+@app.route('/upload')
+def upload():
+    return render_template("upload_new_topic.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
